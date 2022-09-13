50642d2eefd5d271db94d9110e13a95264adffc2
wiekusviljoen <wiekusviljoen@gmail.com>
2022-09-13 20:40:03 +0200

    commit on master

--- src/screens/CoinDetailedScreen/index.jsx
+++ src/screens/CoinDetailedScreen/index.jsx
@@ -8,17 +8,20 @@ const CoinDetailedScreen = () => {
     image: { small },
     name,
     symbol,
-    market_data: { market_cap_rank },
+    market_data: { market_cap_rank, current_price },
   } = Coin;
 
   return (
-    <View>
+    <View style={{ paddingHorizontal: 10 }}>
       <CoinDetailedHeader
         image={small}
-        name={name}
         symbol={symbol}
         marketCapRank={market_cap_rank}
       />
+      <View>
+        <Text style={{ color: "white" }}>{name}</Text>
+        <Text style={{ color: "white" }}>{current_price.usd}</Text>
+      </View>
     </View>
   );
 };
