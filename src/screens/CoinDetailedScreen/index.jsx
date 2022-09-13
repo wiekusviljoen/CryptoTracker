f7ac35f9a6ef531b3d2fe4401f9570ebb16c0c18
wiekusviljoen <wiekusviljoen@gmail.com>
2022-09-13 21:09:35 +0200

    commit on master

--- src/screens/CoinDetailedScreen/index.jsx
+++ src/screens/CoinDetailedScreen/index.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import { View, Text } from "react-native";
 import Coin from "../../../assets/data/crypto.json";
 import CoinDetailedHeader from "./components/CoinDetailedHeader";
+import styles from "./styles";
 
 const CoinDetailedScreen = () => {
   const {
     image: { small },
     name,
     symbol,
-    market_data: { market_cap_rank, current_price },
+    market_data: {
+      market_cap_rank,
+      current_price,
+      price_change_percentage_24h,
+    },
   } = Coin;
 
   return (
@@ -18,9 +23,14 @@ const CoinDetailedScreen = () => {
         symbol={symbol}
         marketCapRank={market_cap_rank}
       />
-      <View>
-        <Text style={{ color: "white" }}>{name}</Text>
-        <Text style={{ color: "white" }}>{current_price.usd}</Text>
+      <View style={styles.priceContainer}>
+        <View>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.currentPrice}>${current_price.usd}</Text>
+        </View>
+        <Text style={{ color: "white", fontSize: 17 }}>
+          {price_change_percentage_24h.toFixed(2)}%
+        </Text>
       </View>
     </View>
   );
---
+++ src/screens/CoinDetailedScreen/styles.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  currentPrice: {
+    color: "white",
+    fontSize: 30,
+    fontWeight: "600",
+    letterSpacing: 1,
+  },
+  name: {
+    color: "white",
+    fontSize: 15,
+  },
+  priceContainer: {
+    padding: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
+
+export default styles;
