79c74c712f334fad547ac03f5df6a8487efe133b
wiekusviljoen <wiekusviljoen@gmail.com>
2022-09-13 20:31:09 +0200

    commit on master

--- src/screens/CoinDetailedScreen/index.jsx
+++ src/screens/CoinDetailedScreen/index.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
-import { Ionicons, EvilIcons } from "@expo/vector-icons";
+import { View, Text } from "react-native";
 import Coin from "../../../assets/data/crypto.json";
-import styles from "./styles";
+import CoinDetailedHeader from "./components/CoinDetailedHeader";
 
 const CoinDetailedScreen = () => {
   const {
@@ -13,17 +12,13 @@ const CoinDetailedScreen = () => {
   } = Coin;
 
   return (
-    <View style={styles.headerContainer}>
-      <Ionicons name="arrow-back-sharp" size={30} color="white" />
-      <Image
-        source={{
-          uri: small,
-        }}
-        style={{ width: 25, height: 25 }}
+    <View>
+      <CoinDetailedHeader
+        image={small}
+        name={name}
+        symbol={symbol}
+        marketCapRank={market_cap_rank}
       />
-      <Text style={{ color: "white" }}>{symbol.toUpperCase()}</Text>
-      <Text style={{ color: "white" }}>#{market_cap_rank}</Text>
-      <EvilIcons name="user" size={30} color="white" />
     </View>
   );
 };
